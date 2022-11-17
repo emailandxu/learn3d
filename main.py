1a66d0b2a9587a45343ec272c415061224975b32
tony <emailandxu@qq.com>
2022-11-17 11:28:48 +0800

    test meshplot offline feature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import meshplot as mp
 
+mp.offline()
+
 data = np.load('data.npz')
 v, f, n, fs = data["v"], data["f"], data["n"], data["fs"]
 v1, f1, v2, f2 = data["v1"], data["f1"], data["v2"], data["f2"]
